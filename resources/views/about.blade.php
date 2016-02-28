27a0ddf1de3d1fa9ce72fef77800a67204b5323a
Sebastjan Koci <sebastjan.koci@gmail.com>
2016-02-28 19:20:48 +0100

    blog

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends("base")
+
+@section("tab_title")
+
+    About me
+
+@endsection
+
+@section("page_title")
+
+    About me
+
+@endsection
+
+@section("small_page_title")
+
+    Who am I?
+
+@endsection
+
+@section("content")
+
+
+
+@endsection
